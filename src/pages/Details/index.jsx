8ce1e42cfaa67696abea6a32dd4303869bb2230c
Brenobn <brenobragabn@gmail.com>
2023-12-06 18:45:46 -0300

    Adding more sessions to the Details page

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -32,7 +32,29 @@ export function Details() {
           </div>
         </Section>
 
-        
+        <Section 
+          description={` Pragas nas colheitas fizeram a civilização humana regredir para uma sociedade agrária em futuro de data desconhecida. Cooper, ex-piloto da NASA, tem uma fazenda com sua família. Murphy, a filha de dez anos de Cooper, acredita que seu quarto está assombrado por um fantasma que tenta se comunicar com ela. Pai e filha descobrem que o "fantasma" é uma inteligência desconhecida que está enviando mensagens codificadas através de radiação gravitacional, deixando coordenadas em binário que os levam até uma instalação secreta da NASA liderada pelo professor John Brand.
+          ...
+          Cooper e Amelia descobrem que 23 anos se passaram.`} 
+        >
+          <div className="TagsWrapper">
+          <Tag text="Ação" />
+          <Tag text="Aventura" />
+          <Tag text="Romance" />
+          </div>
+        </Section>
+
+        <Section 
+          description={` Pragas nas colheitas fizeram a civilização humana regredir para uma sociedade agrária em futuro de data desconhecida. Cooper, ex-piloto da NASA, tem uma fazenda com sua família. Murphy, a filha de dez anos de Cooper, acredita que seu quarto está assombrado por um fantasma que tenta se comunicar com ela. Pai e filha descobrem que o "fantasma" é uma inteligência desconhecida que está enviando mensagens codificadas através de radiação gravitacional, deixando coordenadas em binário que os levam até uma instalação secreta da NASA liderada pelo professor John Brand.
+          ...
+          Cooper e Amelia descobrem que 23 anos se passaram.`} 
+        >
+          <div className="TagsWrapper">
+          <Tag text="Ação" />
+          <Tag text="Aventura" />
+          <Tag text="Romance" />
+          </div>
+        </Section>
       </div>
     </Container>
   );
