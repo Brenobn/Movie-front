e5ee7365ddb54976f7a5bc7cf9b65427cb62b8fe
Brenobn <brenobragabn@gmail.com>
2023-12-06 10:39:24 -0300

    Creating new Input component with HTML structure and style

---
+++ src/components/Input/styles.js
@@ -0,0 +1,31 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  width: 100%;
+  display: flex;
+  align-items: center;
+
+  background-color: ${({ theme }) => theme.COLORS.BACKGROUND_700};
+  color: ${({ theme }) => theme.COLORS.GRAY_200};
+
+  border-radius: 10px;
+
+  > input {
+    width: 100%;
+    height: 56px;
+
+    padding: 19px 24px;
+
+    color: ${({ theme }) => theme.COLORS.WHITE};
+    background: transparent;
+    border: 0;
+
+    &:placeholder {
+      color: ${({ theme }) => theme.COLORS.GRAY_200};
+    }
+  }
+
+  > svg {
+    margin-left: 16px;
+  }
+`;
