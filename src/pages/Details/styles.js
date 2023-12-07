089fbd7225f2b5bee9f3aeb7c807aaf2ad794201
Brenobn <brenobragabn@gmail.com>
2023-12-07 19:11:46 -0300

    Creating new Details page with HTML structure and style

--- src/pages/Details/styles.js
+++ src/pages/Details/styles.js
@@ -1,54 +1,62 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-  max-width: 1366px;
-  max-height: 1024px;
-  
+  max-width: 893px;
+  background-color: ${({ theme }) => theme.COLORS.BACKGROUND_800};
+
   display: grid;
   grid-template-rows: 116px auto;
-  grid-template-areas:
+  grid-template-areas: 
   "header"
   "content";
 
-  .Sections {
-      display: inline-flex;
-      justify-content: center;
-      flex-direction: column;
-      align-items: center;
-      gap: 24px;
-      width: 100%;
-    }
+`;
 
-    .TagsWrapper {
-      display: flex;
-      gap: 8px;
-      margin-top: 15px;
-    }
+export const ContentContainer = styled.div`
+  max-width: 1137px;
+  max-height: 581px;
 
-  .headofpage {
-    display: flex;
-    justify-content: space-between;
-    gap: 710px;
-    margin: 50px 170px 40px 190px;
+  margin: 40px auto;
 
+  background-color: ${({ theme }) => theme.COLORS.BACKGROUND_800};
 
-    > h1 {
-      color: ${({ theme }) => theme.COLORS.WHITE};
+  display: inline-flex;
+  padding-right: 24px;
+  flex-direction: column;
+  align-items: flex-start;
+  gap: 40px;
+
+  .WayBack {
+    display: inline-flex;
+    align-items: center;
+    gap: 8px;
+
+    a {
+      color: ${({ theme }) => theme.COLORS.SALMON};
+      text-align: center;
       font-family: 'Roboto Slab', sans-serif;
-      font-size: 32px;
+      font-size: 16px;
       font-weight: 400;
     }
 
-    > button {
-      display: inline-flex;
-      height: 48px;
-      width: 207px;
-      align-items: center;
-      gap: 8px;
-      flex-shrink: 0;
-      padding: 30px;
-      
-      margin: 13px 32px 13px 52px;
+    > svg {
+      color: ${({ theme }) => theme.COLORS.SALMON};
     }
+  }
+
+
+  h1 {
+    color: ${({ theme }) => theme.COLORS.WHITE};
+    font-family: 'Roboto Slab', sans-serif;
+    font-size: 36px;
+    font-weight: 500; 
+  }
+
+  p {
+    color: ${({ theme }) => theme.COLORS.WHITE};
+    text-align: justify;
+    font-family: 'Roboto Slab', sans-serif;
+    font-size: 16px;
+    font-weight: 400;
   }
 `;
