d109b9d9882a9cecae11d94a371439652bfed17b
Brenobn <brenobragabn@gmail.com>
2023-12-12 18:29:39 -0300

    Using the display grid strategy to keep the Header fixed on the Home page

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,4 +1,4 @@
-import { Container } from "./styles";
+import { Container, Content } from "./styles";
 
 import { FiPlus } from "react-icons/fi";
  
@@ -11,54 +11,57 @@ export function Home() {
   return(
     <Container>
       <Header />
+      <main>
+        <Content>
+          <div className="headofpage">
+            <h1>Meus filmes</h1>
 
-      <div className="headofpage">
-        <h1>Meus filmes</h1>
+            <Button 
+              icon={FiPlus} 
+              title="Adicionar filme" 
+            />
 
-        <Button 
-          icon={FiPlus} 
-          title="Adicionar filme" 
-        />
-
-      </div>
-
-      <div className="Sections">
-        <Section 
-          description={` Pragas nas colheitas fizeram a civilização humana regredir para uma sociedade agrária em futuro de data desconhecida. Cooper, ex-piloto da NASA, tem uma fazenda com sua família. Murphy, a filha de dez anos de Cooper, acredita que seu quarto está assombrado por um fantasma que tenta se comunicar com ela. Pai e filha descobrem que o "fantasma" é uma inteligência desconhecida que está enviando mensagens codificadas através de radiação gravitacional, deixando coordenadas em binário que os levam até uma instalação secreta da NASA liderada pelo professor John Brand.
-          ...
-          Cooper e Amelia descobrem que 23 anos se passaram.`} 
-        >
-          <div className="TagsWrapper">
-          <Tag text="Ação" />
-          <Tag text="Aventura" />
-          <Tag text="Romance" />
           </div>
-        </Section>
 
-        <Section 
-          description={` Pragas nas colheitas fizeram a civilização humana regredir para uma sociedade agrária em futuro de data desconhecida. Cooper, ex-piloto da NASA, tem uma fazenda com sua família. Murphy, a filha de dez anos de Cooper, acredita que seu quarto está assombrado por um fantasma que tenta se comunicar com ela. Pai e filha descobrem que o "fantasma" é uma inteligência desconhecida que está enviando mensagens codificadas através de radiação gravitacional, deixando coordenadas em binário que os levam até uma instalação secreta da NASA liderada pelo professor John Brand.
-          ...
-          Cooper e Amelia descobrem que 23 anos se passaram.`} 
-        >
-          <div className="TagsWrapper">
-          <Tag text="Ação" />
-          <Tag text="Aventura" />
-          <Tag text="Romance" />
-          </div>
-        </Section>
+          <div className="Sections">
+            <Section 
+              description={` Pragas nas colheitas fizeram a civilização humana regredir para uma sociedade agrária em futuro de data desconhecida. Cooper, ex-piloto da NASA, tem uma fazenda com sua família. Murphy, a filha de dez anos de Cooper, acredita que seu quarto está assombrado por um fantasma que tenta se comunicar com ela. Pai e filha descobrem que o "fantasma" é uma inteligência desconhecida que está enviando mensagens codificadas através de radiação gravitacional, deixando coordenadas em binário que os levam até uma instalação secreta da NASA liderada pelo professor John Brand.
+              ...
+              Cooper e Amelia descobrem que 23 anos se passaram.`} 
+            >
+              <div className="TagsWrapper">
+              <Tag text="Ação" />
+              <Tag text="Aventura" />
+              <Tag text="Romance" />
+              </div>
+            </Section>
 
-        <Section 
-          description={` Pragas nas colheitas fizeram a civilização humana regredir para uma sociedade agrária em futuro de data desconhecida. Cooper, ex-piloto da NASA, tem uma fazenda com sua família. Murphy, a filha de dez anos de Cooper, acredita que seu quarto está assombrado por um fantasma que tenta se comunicar com ela. Pai e filha descobrem que o "fantasma" é uma inteligência desconhecida que está enviando mensagens codificadas através de radiação gravitacional, deixando coordenadas em binário que os levam até uma instalação secreta da NASA liderada pelo professor John Brand.
-          ...
-          Cooper e Amelia descobrem que 23 anos se passaram.`} 
-        >
-          <div className="TagsWrapper">
-          <Tag text="Ação" />
-          <Tag text="Aventura" />
-          <Tag text="Romance" />
+            <Section 
+              description={` Pragas nas colheitas fizeram a civilização humana regredir para uma sociedade agrária em futuro de data desconhecida. Cooper, ex-piloto da NASA, tem uma fazenda com sua família. Murphy, a filha de dez anos de Cooper, acredita que seu quarto está assombrado por um fantasma que tenta se comunicar com ela. Pai e filha descobrem que o "fantasma" é uma inteligência desconhecida que está enviando mensagens codificadas através de radiação gravitacional, deixando coordenadas em binário que os levam até uma instalação secreta da NASA liderada pelo professor John Brand.
+              ...
+              Cooper e Amelia descobrem que 23 anos se passaram.`} 
+            >
+              <div className="TagsWrapper">
+              <Tag text="Ação" />
+              <Tag text="Aventura" />
+              <Tag text="Romance" />
+              </div>
+            </Section>
+
+            <Section 
+              description={` Pragas nas colheitas fizeram a civilização humana regredir para uma sociedade agrária em futuro de data desconhecida. Cooper, ex-piloto da NASA, tem uma fazenda com sua família. Murphy, a filha de dez anos de Cooper, acredita que seu quarto está assombrado por um fantasma que tenta se comunicar com ela. Pai e filha descobrem que o "fantasma" é uma inteligência desconhecida que está enviando mensagens codificadas através de radiação gravitacional, deixando coordenadas em binário que os levam até uma instalação secreta da NASA liderada pelo professor John Brand.
+              ...
+              Cooper e Amelia descobrem que 23 anos se passaram.`} 
+            >
+              <div className="TagsWrapper">
+              <Tag text="Ação" />
+              <Tag text="Aventura" />
+              <Tag text="Romance" />
+              </div>
+            </Section>
           </div>
-        </Section>
-      </div>
+        </Content>
+      </main>
     </Container>
   );
 }
