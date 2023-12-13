2c4a7d9c8e9edffcf8c1702315086bbd68a5a2ba
Brenobn <brenobragabn@gmail.com>
2023-12-13 12:22:59 -0300

    Adding Scroll to Global Style

--- src/styles/global.js
+++ src/styles/global.js
@@ -33,4 +33,24 @@ export default createGlobalStyle`
   button:hover, a:hover {
     filter: brightness(0.9);
   }
+
+  *{--sb-track-color: transparent;
+    --sb-thumb-color: ${({ theme }) => theme.COLORS.SALMON};
+    --sb-size: 8px;
+  }  
+
+  *::-webkit-scrollbar {
+    width: 8px; 
+    height: 8px;
+  }
+
+  *::-webkit-scrollbar-track {
+    background: transparent;
+    border-radius: 8px;
+  }
+
+  *::-webkit-scrollbar-thumb {
+    background: ${({ theme }) => theme.COLORS.SALMON};
+    border-radius: 8px;
+  }
 `;
