d6c8058fa16f415ab7fef1ff206ffd4f7270f19d
Brenobn <brenobragabn@gmail.com>
2023-12-15 17:10:28 -0300

    Changing the CSS style for styled-components semantic tags

--- src/pages/Details/styles.js
+++ src/pages/Details/styles.js
@@ -1,21 +1,20 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-  
   background-color: ${({ theme }) => theme.COLORS.BACKGROUND_800};
 
   display: grid;
   grid-template-rows: 116px auto;
-  grid-template-areas: 
-  "header"
-  "content";
+  grid-template-areas:
+    "header"
+    "content";
 
   > main {
     grid-area: content;
     overflow-y: auto;
 
     height: calc(100vh - 116px);
-    
+
     width: 100%;
   }
 `;
@@ -30,50 +29,54 @@ export const Content = styled.div`
   display: flex;
   flex-direction: column;
   gap: 40px;
+`;
 
-  .WayBack {
-    display: flex;
-    align-items: center;
-    gap: 8px;
+export const WayBack = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 8px;
 
-    a {
-      color: ${({ theme }) => theme.COLORS.SALMON};
-      text-align: center;
-      font-family: 'Roboto Slab', sans-serif;
-      font-size: 16px;
-      font-weight: 400;
-    }
-
-    > svg {
-      color: ${({ theme }) => theme.COLORS.SALMON};
-    }
+  svg {
+    color: ${({ theme }) => theme.COLORS.SALMON};
   }
+`;
 
-  .StarWrapper {
-    display: inline-flex;
-    flex-direction: row;
-    align-items: center;
+export const LinkToGoOut = styled.a`
+  color: ${({ theme }) => theme.COLORS.SALMON};
+  text-align: center;
+  font-family: "Roboto Slab", sans-serif;
+  font-size: 16px;
+  font-weight: 400;
 
-    h1 {
-      margin-right: 19px;
-      color: ${({ theme }) => theme.COLORS.WHITE};
-      font-family: 'Roboto Slab', sans-serif;
-      font-size: 36px;
-      font-weight: 500; 
-    }
+  svg {
+    color: ${({ theme }) => theme.COLORS.SALMON};
   }
+`;
 
-  .TagWrapper {
-    display: inline-flex;
-    align-items: flex-start;
-    gap: 8px;
-  }
+export const TagWrapper = styled.div`
+  display: inline-flex;
+  align-items: flex-start;
+  gap: 8px;
+`;
 
-  p {
-    color: ${({ theme }) => theme.COLORS.WHITE};
-    text-align: justify;
-    font-family: 'Roboto Slab', sans-serif;
-    font-size: 16px;
-    font-weight: 400;
-  }
+export const Paragraph = styled.p`
+  color: ${({ theme }) => theme.COLORS.WHITE};
+  text-align: justify;
+  font-family: "Roboto Slab", sans-serif;
+  font-size: 16px;
+  font-weight: 400;
+`;
+
+export const StarsWrapper = styled.div`
+  display: inline-flex;
+  flex-direction: row;
+  align-items: center;
+`;
+
+export const Title = styled.h1`
+  margin-right: 19px;
+  color: ${({ theme }) => theme.COLORS.WHITE};
+  font-family: "Roboto Slab", sans-serif;
+  font-size: 36px;
+  font-weight: 500;
 `;
