4b2b2657b9ab4385953f95b88db88868a70cab60
Brenobn <brenobragabn@gmail.com>
2023-12-23 14:02:35 -0300

    Importing useState

--- src/pages/SignUp/index.jsx
+++ src/pages/SignUp/index.jsx
@@ -8,6 +8,8 @@ import {
   Text,
 } from "./styles";
 
+import { useState } from "react";
+
 import { Input } from "../../components/Input";
 import { Button } from "../../components/Button";
 import { ButtonText } from "../../components/ButtonText";
