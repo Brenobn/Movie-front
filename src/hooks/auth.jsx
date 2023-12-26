2b215f89f312ec8746d255c10f04bc7c964da180
Brenobn <brenobragabn@gmail.com>
2023-12-26 12:21:06 -0300

    Creating async function signIn with handling taking information from the api

--- src/hooks/auth.jsx
+++ src/hooks/auth.jsx
@@ -5,10 +5,21 @@ import { api } from "../services/api";
 export const AuthContext = createContext({});
 
 function AuthProvider({ children }) {
+  async function signIn({ email, password }) {
+    try {
+      const response = await api.post("/sessions", { email, password });
+      console.log(response);
+    } catch (error) {
+      if (error.response) {
+        alert(error.response.data.message);
+      } else {
+        alert("Não foi possivel entrar.");
+      }
+    }
+  }
+
   return (
-    <AuthContext.Provider value={{ email: "brenobragabn@gmil.com" }}>
-      {children}
-    </AuthContext.Provider>
+    <AuthContext.Provider value={{ signIn }}>{children}</AuthContext.Provider>
   );
 }
 
