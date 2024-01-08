e5faf3a24f9ee1d1fbcb3226fc53f2dcc521bef4
Brenobn <brenobragabn@gmail.com>
2024-01-08 09:46:52 -0300

    Creating updateProfile function using localStorage, extracting user data from the api

--- src/hooks/auth.jsx
+++ src/hooks/auth.jsx
@@ -34,6 +34,22 @@ function AuthProvider({ children }) {
     setData({});
   }
 
+  async function updateProfile({ user }) {
+    try {
+      await api.put("/users", user);
+      localStorage.setItem("@movie-front:user", JSON.stringify(user));
+
+      setData({ user, token: data.token });
+      alert("Perfil atualizado!");
+    } catch (error) {
+      if (error.response) {
+        alert(error.response.data.message);
+      } else {
+        alert("Não foi possivel atualizar o perfil.");
+      }
+    }
+  }
+
   useEffect(() => {
     const token = localStorage.getItem("@movie-front:token");
     const user = localStorage.getItem("@movie-front:user");
