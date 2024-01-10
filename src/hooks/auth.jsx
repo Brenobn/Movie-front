e6b892fa6512ea45a14a99b182c4eb9036afffc9
Brenobn <brenobragabn@gmail.com>
2024-01-10 11:32:52 -0300

    Passing avatarFile to updateProfile function and creating a condition if file exists

--- src/hooks/auth.jsx
+++ src/hooks/auth.jsx
@@ -34,8 +34,16 @@ function AuthProvider({ children }) {
     setData({});
   }
 
-  async function updateProfile({ user }) {
+  async function updateProfile({ user, avatarFile }) {
     try {
+      if (avatarFile) {
+        const fileUploadForm = new Formdata();
+        fileUploadForm.append("avatar", avatarFile);
+
+        const response = await api.patch("/users/avatar", fileUploadForm);
+        user.avatar = response.data.avatar;
+      }
+
       await api.put("/users", user);
       localStorage.setItem("@movie-front:user", JSON.stringify(user));
 
