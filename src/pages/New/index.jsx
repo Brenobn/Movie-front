8a35ca22cbb394297405e8cee866fb6b8b783aee
Brenobn <brenobragabn@gmail.com>
2024-01-11 12:09:52 -0300

    Creating state for title, rating and description

--- src/pages/New/index.jsx
+++ src/pages/New/index.jsx
@@ -21,6 +21,10 @@ import { Button } from "../../components/Button";
 import { NoteItem } from "../../components/NoteItem";
 
 export function New() {
+  const [title, setTitle] = useState("");
+  const [rating, setRating] = useState(0);
+  const [description, setDescription] = useState("");
+
   const [tags, setTags] = useState([]);
   const [newTag, setNewTag] = useState("");
 
@@ -43,10 +47,19 @@ export function New() {
         </WayBack>
         <Title>Novo filme</Title>
         <Col2>
-          <Input placeholder="Título" />
-          <Input placeholder="Sua nota (de 0 a 5)" />
+          <Input
+            placeholder="Título"
+            onChange={(e) => setTitle(e.target.value)}
+          />
+          <Input
+            placeholder="Sua nota (de 0 a 5)"
+            onChange={(e) => setRating(e.target.value)}
+          />
         </Col2>
-        <textarea placeholder="Observações"></textarea>
+        <textarea
+          placeholder="Observações"
+          onChange={(e) => setDescription(e.target.value)}
+        ></textarea>
         <Section>
           <h4>Marcadores</h4>
           <Children>
