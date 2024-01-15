de501b8c1d8cde71cc7b3d964fabda91992b16ef
Brenobn <brenobragabn@gmail.com>
2024-01-15 11:24:21 -0300

    Using map to dynamically create a new movie list

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -49,43 +49,19 @@ export function Home() {
             </ButtonHome>
           </HeadOfPage>
 
-          <Sections>
-            <Section
-              description={` Pragas nas colheitas fizeram a civilização humana regredir para uma sociedade agrária em futuro de data desconhecida. Cooper, ex-piloto da NASA, tem uma fazenda com sua família. Murphy, a filha de dez anos de Cooper, acredita que seu quarto está assombrado por um fantasma que tenta se comunicar com ela. Pai e filha descobrem que o "fantasma" é uma inteligência desconhecida que está enviando mensagens codificadas através de radiação gravitacional, deixando coordenadas em binário que os levam até uma instalação secreta da NASA liderada pelo professor John Brand.
-              ...
-              Cooper e Amelia descobrem que 23 anos se passaram.`}
-            >
-              <TagsWrapper>
-                <Tag text="Ação" />
-                <Tag text="Aventura" />
-                <Tag text="Romance" />
-              </TagsWrapper>
-            </Section>
-
-            <Section
-              description={` Pragas nas colheitas fizeram a civilização humana regredir para uma sociedade agrária em futuro de data desconhecida. Cooper, ex-piloto da NASA, tem uma fazenda com sua família. Murphy, a filha de dez anos de Cooper, acredita que seu quarto está assombrado por um fantasma que tenta se comunicar com ela. Pai e filha descobrem que o "fantasma" é uma inteligência desconhecida que está enviando mensagens codificadas através de radiação gravitacional, deixando coordenadas em binário que os levam até uma instalação secreta da NASA liderada pelo professor John Brand.
-              ...
-              Cooper e Amelia descobrem que 23 anos se passaram.`}
-            >
-              <TagsWrapper>
-                <Tag text="Ação" />
-                <Tag text="Aventura" />
-                <Tag text="Romance" />
-              </TagsWrapper>
-            </Section>
-
-            <Section
-              description={` Pragas nas colheitas fizeram a civilização humana regredir para uma sociedade agrária em futuro de data desconhecida. Cooper, ex-piloto da NASA, tem uma fazenda com sua família. Murphy, a filha de dez anos de Cooper, acredita que seu quarto está assombrado por um fantasma que tenta se comunicar com ela. Pai e filha descobrem que o "fantasma" é uma inteligência desconhecida que está enviando mensagens codificadas através de radiação gravitacional, deixando coordenadas em binário que os levam até uma instalação secreta da NASA liderada pelo professor John Brand.
-              ...
-              Cooper e Amelia descobrem que 23 anos se passaram.`}
-            >
-              <TagsWrapper>
-                <Tag text="Ação" />
-                <Tag text="Aventura" />
-                <Tag text="Romance" />
-              </TagsWrapper>
-            </Section>
-          </Sections>
+          {movies.map((movie, index) => {
+            return (
+              <Sections key={index} onClick={() => handleDetails(movie.id)}>
+                <Section title={movie.title} description={movie.description}>
+                  <TagsWrapper>
+                    <Tag text="Ação" />
+                    <Tag text="Aventura" />
+                    <Tag text="Romance" />
+                  </TagsWrapper>
+                </Section>
+              </Sections>
+            );
+          })}
         </Content>
       </main>
     </Container>
