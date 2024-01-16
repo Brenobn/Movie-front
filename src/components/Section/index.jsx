ad3fecf4367060784eab5bf32b6e6ce61c4894dd
Brenobn <brenobragabn@gmail.com>
2024-01-16 11:24:30 -0300

    Importing useState, creating state for rating and hover. And adding some parameters to function

--- src/components/Section/index.jsx
+++ src/components/Section/index.jsx
@@ -1,18 +1,40 @@
+import { useState } from "react";
+
 import { MdStar, MdStarBorder } from "react-icons/md";
 
 import { Container, Stars, Paragraph, Title } from "./styles";
 
-export function Section({ children, description, title }) {
+export function Section({
+  children,
+  description,
+  title,
+  resize = 20,
+  numStars = 0,
+  ...rest
+}) {
+  const [rating, setRating] = useState(0);
+  const [hover, setHover] = useState(0);
+
   return (
     <Container to="/details/5">
       <Title>{title}</Title>
-      <Stars>
-        <MdStar />
-        <MdStar />
-        <MdStar />
-        <MdStar />
-        <MdStarBorder />
-      </Stars>
+      {[...Array(5)].map((star, index) => {
+        index += 1;
+        return (
+          <Stars
+            key={index}
+            onClick={() => setRating(index)}
+            onMouseEnter={() => setHover(index)}
+            onMouseLeave={() => setHover(rating)}
+          >
+            {index <= numStars ? (
+              <MdStar size={resize} />
+            ) : (
+              <MdStarBorder size={resize} />
+            )}
+          </Stars>
+        );
+      })}
       <Paragraph>{description}</Paragraph>
 
       {children}
