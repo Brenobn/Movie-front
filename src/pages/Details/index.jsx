cf59578d29c31eb8f44936af10157e0745ea1e8e
Brenobn <brenobragabn@gmail.com>
2024-01-18 11:50:51 -0300

    Importing StarRating component and using map to scroll through tags creating array with registered tags

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,7 +1,6 @@
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { GoArrowLeft } from "react-icons/go";
-import { MdStar, MdStarBorder } from "react-icons/md";
 
 import { api } from "../../services/api";
 
@@ -18,6 +17,7 @@ import {
 import { Header } from "../../components/Header";
 import { Tag } from "../../components/Tag";
 import { ButtonText } from "../../components/ButtonText";
+import { StarRating } from "../../components/StarRating";
 
 export function Details() {
   const [data, setData] = useState(null);
@@ -36,60 +36,28 @@ export function Details() {
   return (
     <Container>
       <Header />
-
-      <main>
-        <Content>
-          <WayBack>
-            <GoArrowLeft />
-            <ButtonText title="Voltar" to="/" />
-          </WayBack>
-          <StarsWrapper>
-            <Title>Interestellar</Title>
-            <MdStar />
-            <MdStar />
-            <MdStar />
-            <MdStar />
-            <MdStarBorder />
-          </StarsWrapper>
-          <TagWrapper>
-            <Tag text="Ficção Científica" />
-            <Tag text="Drama" />
-            <Tag text="Familia" />
-          </TagWrapper>
-          <Paragraph>
-            Pragas nas colheitas fizeram a civilização humana regredir para uma
-            sociedade agrária em futuro de data desconhecida. Cooper, ex-piloto
-            da NASA, tem uma fazenda com sua família. Murphy, a filha de dez
-            anos de Cooper, acredita que seu quarto está assombrado por um
-            fantasma que tenta se comunicar com ela. Pai e filha descobrem que o
-            "fantasma" é uma inteligência desconhecida que está enviando
-            mensagens codificadas através de radiação gravitacional, deixando
-            coordenadas em binário que os levam até uma instalação secreta da
-            NASA liderada pelo professor John Brand. O cientista revela que um
-            buraco de minhoca foi aberto perto de Saturno e que ele leva a
-            planetas que podem oferecer condições de sobrevivência para a
-            espécie humana. As "missões Lázaro" enviadas anos antes
-            identificaram três planetas potencialmente habitáveis orbitando o
-            buraco negro Gargântua: Miller, Edmunds e Mann – nomeados em
-            homenagem aos astronautas que os pesquisaram. Brand recruta Cooper
-            para pilotar a nave espacial Endurance e recuperar os dados dos
-            astronautas; se um dos planetas se mostrar habitável, a humanidade
-            irá seguir para ele na instalação da NASA, que é na realidade uma
-            enorme estação espacial. A partida de Cooper devasta Murphy. Além de
-            Cooper, a tripulação da Endurance é formada pela bióloga Amelia,
-            filha de Brand; o cientista Romilly, o físico planetário Doyle, além
-            dos robôs TARS e CASE. Eles entram no buraco de minhoca e se dirigem
-            a Miller, porém descobrem que o planeta possui enorme dilatação
-            gravitacional temporal por estar tão perto de Gargântua: cada hora
-            na superfície equivale a sete anos na Terra. Eles entram em Miller e
-            descobrem que é inóspito já que é coberto por um oceano raso e
-            agitado por ondas enormes. Uma onda atinge a tripulação enquanto
-            Amelia tenta recuperar os dados de Miller, matando Doyle e atrasando
-            a partida. Ao voltarem para a Endurance, Cooper e Amelia descobrem
-            que 23 anos se passaram.
-          </Paragraph>
-        </Content>
-      </main>
+      {data && (
+        <main>
+          <Content>
+            <WayBack>
+              <GoArrowLeft />
+              <ButtonText title="Voltar" to="/" />
+            </WayBack>
+            <StarsWrapper>
+              <Title>{data.title}</Title>
+              <StarRating numStar={data.rating} />
+            </StarsWrapper>
+            {data.tags && (
+              <TagWrapper>
+                {data.tags.map((tag) => (
+                  <Tag key={String(tag.id)} text={tag.name} />
+                ))}
+              </TagWrapper>
+            )}
+            <Paragraph>{data.description}</Paragraph>
+          </Content>
+        </main>
+      )}
     </Container>
   );
 }
