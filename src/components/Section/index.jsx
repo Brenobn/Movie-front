339ca0fc8e01da3a3ae1c76e5f84f4a31e473f3e
Brenobn <brenobragabn@gmail.com>
2024-01-18 10:53:03 -0300

    Making some changes to the HTML structure of the Section component

--- src/components/Section/index.jsx
+++ src/components/Section/index.jsx
@@ -1,41 +1,9 @@
-import { useState } from "react";
+import { Container, Title } from "./styles";
 
-import { MdStar, MdStarBorder } from "react-icons/md";
-
-import { Container, Stars, Paragraph, Title } from "./styles";
-
-export function Section({
-  children,
-  description,
-  title,
-  resize = 20,
-  numStars = 0,
-  ...rest
-}) {
-  const [rating, setRating] = useState(0);
-  const [hover, setHover] = useState(0);
-
+export function Section({ children, title, ...rest }) {
   return (
     <Container to="/details/5">
       <Title>{title}</Title>
-      {[...Array(5)].map((star, index) => {
-        index += 1;
-        return (
-          <Stars
-            key={index}
-            onClick={() => setRating(index)}
-            onMouseEnter={() => setHover(index)}
-            onMouseLeave={() => setHover(rating)}
-          >
-            {index <= numStars ? (
-              <MdStar size={resize} />
-            ) : (
-              <MdStarBorder size={resize} />
-            )}
-          </Stars>
-        );
-      })}
-      <Paragraph>{description}</Paragraph>
 
       {children}
     </Container>
